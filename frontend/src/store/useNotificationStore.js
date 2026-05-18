7c82a945126e6c77d53e92d8a18bb898277835cd
Qwanzo <pansiluinc@yahoo.com>
2026-05-18 11:18:31 +0530

    Add skipWaiting and convert VAPID key for push
    Call self.skipWaiting() in the service worker so newly installed SWs activate immediately. In the notification store, add a urlBase64ToUint8Array helper and use it to convert the VAPID public key to a Uint8Array before passing it to pushManager.subscribe (applicationServerKey must be a Uint8Array). This fixes push subscription issues and ensures the service worker takes control promptly.

--- frontend/src/store/useNotificationStore.js
+++ frontend/src/store/useNotificationStore.js
@@ -4,7 +4,20 @@ import { useAuthStore } from "./useAuthStore";
 import { useErrorStore } from "./useErrorStore";
 import toast from "react-hot-toast";
 
-const VAPID_PUBLIC_KEY = import.meta.env.VITE_VAPID_PUBLIC_KEY;
+const urlBase64ToUint8Array = (base64String) => {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
 
 export const useNotificationStore = create((set, get) => ({
   notifications: [],
@@ -119,7 +132,7 @@ export const useNotificationStore = create((set, get) => ({
       const registration = await navigator.serviceWorker.ready;
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: publicKey,
+        applicationServerKey: urlBase64ToUint8Array(publicKey),
       });
 
       await axiosInstance.post('/notifications/push/subscribe', subscription);
